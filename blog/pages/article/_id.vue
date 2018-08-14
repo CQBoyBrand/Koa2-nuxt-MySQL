b7d82ac437e40d92446263da4826bb951732555b
2769536052@qq.com <Brand@2016>
2018-08-14 20:16:19 +0800

    准备添加评论
    准备添加评论

--- blog/pages/article/_id.vue
+++ blog/pages/article/_id.vue
@@ -1,23 +1,82 @@
 <template>
-  <div class="artdetail_container clearfix">
-    <div class="right_mdcontent">
-      <h3 class="artdetail_title">{{artDetail.artTitle}}</h3>
-      <div class="artdetail_post_info">
-        <span>{{artDetail.cdate}}</span><span>post by</span><span>{{artDetail.author}}</span>
-        <span>浏览 {{artDetail.pv}} 次</span>
-      </div>
-      <div class="artdetail_content markdown-body">
-        <div v-html="markdownRender" ref="htmlContent">
+  <div class="artdetail_container ">
+    <div class="detail_body clearfix">
+      <div class="right_mdcontent">
+        <h3 class="artdetail_title">{{artDetail.artTitle}}</h3>
+        <div class="artdetail_post_info">
+          <span>{{artDetail.cdate}}</span><span>post by</span><span>{{artDetail.author}}</span>
+          <span>浏览 {{artDetail.pv}} 次</span>
+        </div>
+        <div class="artdetail_content markdown-body">
+          <div v-html="markdownRender" ref="htmlContent">
 
+          </div>
+          <!--留言区-->
+          <div class="art_comment">
+            <div class="comment_title">我要吐槽</div>
+            <div class="comment_tips">
+              <p style="font-size: 16px;color: #000;font-weight: bold;">提示：</p>
+              <p>昵称必填，用于展示在评论中</p>
+              <p>邮箱必填，不会公开展示，方便及时收到回复</p>
+              <p>网址选填，方便看到的人去访问,请完整填写,例如(http://www.brandhuang.com)</p>
+            </div>
+            <div class="comment_form">
+              <el-form :model="artComment" ref="artComment">
+                <el-row :gutter="10">
+                  <el-col :sm="12" :md="8">
+                    <el-form-item
+                      prop="nickName"
+                      label="昵称(必填)："
+                      :rules="[{ required: true, message: '请输入昵称', trigger: 'blur' }]"
+                    >
+                      <el-input v-model="artComment.nickName"></el-input>
+                    </el-form-item>
+                  </el-col>
+                  <el-col :sm="12" :md="8">
+                    <el-form-item
+                      prop="email"
+                      label="邮箱(必填)："
+                      :rules="[
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+    ]"
+                    >
+                      <el-input v-model="artComment.email"></el-input>
+                    </el-form-item>
+                  </el-col>
+                  <el-col :sm="12" :md="8">
+                    <el-form-item
+                      prop="webUrl"
+                      label="网址(选填)：">
+                      <el-input v-model="artComment.webUrl"></el-input>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+                <el-form-item
+                  label="内容(必填)："
+                  :rules="[{ required: true, message: '请输入评论内容', trigger: 'blur' }]"
+                  prop="comment">
+                  <el-input
+                    type="textarea"
+                    :autosize="{ minRows: 2, maxRows: 4}"
+                    placeholder="请输入内容"
+                    v-model="artComment.comment"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                  <el-button type="primary" :loading="committing" @click="postArtComment('artComment')">提交</el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="left_munu">
-      <div class="menus">
-        目录:
-        <div class="menuNav" ref="menuNav" v-html="markdownToc"></div>
+      <div class="left_munu">
+        <div class="menus">
+          目录:
+          <div class="menuNav" ref="menuNav" v-html="markdownToc"></div>
+        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -88,7 +147,13 @@
     name: 'detail',
     data() {
       return {
-        md: md
+        committing: false,
+        artComment: {
+          email: '',
+          webUrl: '',
+          nickName: '', //昵称
+          comment: ''
+        }
       }
     },
     head() {
@@ -123,6 +188,25 @@
       }
     },
     methods: {
+      // 评论
+      postArtComment(formName) {
+        this.committing = true
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+            setTimeout(() => {
+              this.committing = false
+            },500)
+
+          } else {
+            console.log('error submit!!');
+            setTimeout(() => {
+              this.committing = false;
+            },500)
+            return false;
+          }
+        });
+      },
       navMenu() {
         let nodes = this.$refs.menuNav.children
         if (nodes.length) {
@@ -132,6 +216,7 @@
         }
         let _this = this
         this.$refs.menuNav.style.display = 'block'
+
         function judageH(node, i, nodes) {
           let reg = /^H[1-6]{1}$/;
           if (!reg.exec(node.tagName)) {
@@ -158,12 +243,13 @@
     background-color: #fff;
     padding: 20px;
     @media screen and (max-width: 992px) {
-      .left_munu{
+      .left_munu {
         display: none;
       }
+
       .right_mdcontent {
         float: left;
-        width:100%;
+        width: 100%;
       }
     }
     @media screen and (min-width: 992px) {
@@ -182,7 +268,7 @@
         max-width: 240px;
         border-left: 1px solid #ccc;
         padding-left: 20px;
-        .menuNav{
+        .menuNav {
           display: none;
         }
         h1, h2, h3, h4, h5, h6 {
@@ -245,6 +331,27 @@
         margin: auto;
         text-align: right;
 
+      }
+    }
+
+    .art_comment {
+      margin-top: 30px;
+      padding-top: 30px;
+      border-top: 2px solid #000;
+      .comment_title {
+        text-align: center;
+        font-weight: bold;
+        font-size: 20px;
+      }
+      .comment_tips {
+        font-size: 12px;
+        color: #999;
+        p {
+          margin-bottom: 0;
+        }
+      }
+      .comment_form {
+        padding-top: 30px;
       }
     }
   }
